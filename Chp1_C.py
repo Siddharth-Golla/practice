2779c7b1cc5400555aadbb4dd7665c80ea0ef724
Siddharth-Golla <siddharth.golla87@gmail.com>
2022-01-19 12:38:25 +0530

    C-25

--- Chp1_C.py
+++ Chp1_C.py
@@ -1,5 +1,6 @@
 from random import randint
 from typing import Sequence
+from array import array
 
 # C-14) Write a short Python function that takes a sequence of integer values and
 #   determines if there is a distinct pair of numbers in the sequence whose
@@ -92,3 +93,70 @@ print(shuffle_([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
 # C-21) Write a Python program that repeatedly reads lines from standard input
 # until an EOFError is raised, and then outputs those lines in reverse order
 # (a user can indicate end of input by typing ctrl-D).
+
+# Solve later
+
+
+# C-22) Write a short Python program that takes two arrays a and b of length n
+# storing int values, and returns the dot product of a and b. That is, it returns
+# an array c of length n such that c[i] = a[i] · b[i], for i = 0,...,n− 1.
+
+a = array('i', [1, 2, 3])
+b = array('i', [4, 5, 6])
+c = array('i', [])
+
+for i in range(len(a)):
+    temp = (a[i] * b[i])
+    c.insert(i, temp)
+
+print(c)
+
+
+# C-23) Give an example of a Python code fragment that attempts to write an element to a list based on an index that may be out of bounds.
+# If that index is out of bounds, the program should catch the exception that results, and
+# print the following error message: “Don’t try buffer overflow attacks in Python!”
+
+list2 = []
+try:
+    list2[3] = 10
+except IndexError:
+    print("Don’t try buffer overflow attacks in Python!")
+
+
+# C-24) Write a short Python function that counts the number of vowels in a given character string.
+
+def count_vowel(sample_string: str) -> int:
+    """Counts the number of vowel in a given string
+
+    Args:
+        sample_string (str): Given character string
+
+    Returns:
+        int: Number of vowels in given string
+    """
+    vowel_count = 0
+    for char in sample_string:
+        if char in ["a", 'e', 'o', 'i', 'u']:
+            vowel_count += 1
+    return vowel_count
+
+
+print(count_vowel("Mississippi river is drying up"))
+
+
+# C-25) Write a short Python function that takes a string s, representing a sentence, and returns a copy of the string
+# with all punctuation removed. For example, if given the string "Let's try, Mike.", this function would return "Lets try Mike".
+
+def remove_punc(sample_string: str) -> str:
+    punc = '''!()-[]/{/};:'"\,<>./?@#$%^&*_~'''
+    for char in sample_string:
+        if char in punc:
+            sample_string = sample_string.replace(char, "")
+
+    return sample_string
+
+
+print(remove_punc("Let's try, Mike."))
+
+
+# C-26)
