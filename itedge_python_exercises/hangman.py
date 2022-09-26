723d35005df9a8b87c084ed25e7e902b6c32bc45
Siddharth Golla <siddharth.golla87@gmail.com>
2022-09-26 18:40:17 +0530

    hangman updated

--- itedge_python_exercises/hangman.py
+++ itedge_python_exercises/hangman.py
@@ -1,14 +1,14 @@
 from random import choice
 
 categories = {
-    1: ["Apple", "Banana", "Apricot", "Avocados", "Blueberry", "Cherry", "Fig", "Grapes", "Custard apple", "Dragon fruit", "Guava", "Jackfruit", "Kiwi", "Mango", "Pear", "Oranges", "Papaya", "Peach", "Pomegranate", "Pineapple", "Raspberries", "Strawberries", "Watermelon"],
-    2: ["Computer", "Internet", "Engineering", "Software", "nanotechnology", "application", "system", "robotics", "electronics", "communication", "biotechnology", "automation", "information", "innovation", "game", "code", "technology", "wireless", "hardware", "virtual", "social media", "computer science", "phone", "process", "bionics", "nanotech", "digital", "radio", "value", "developer", "core", "commercial", "development", "aeronautical", "language", "cybersecurity", "methodology", "online", "technician", "consumer", "handheld devices", "machine", "protocol", "architectures", "engineer", "invention", "video"],
-    3: ["Kinetic", "astronomy", "astrophysics", "atom", "beaker", "biochemistry", "biology", "botany","cell","chemical","chemistry","climate","climatologist","data","datum","electricity","electrochemist","element""energy""entomology""evolution""experiment""fossil""genetics""geology""glassware" "cylinder"gravity laboratory hypothesis magnetism mass matter measure meteorologist meteorology microbiologist microbiology microscope mineral mineralogy molecule motion observatory observe organism physical science ],
-    4: ["Mumbai"]
+    1: ["Apple", "Banana", "Apricot", "Avocados", "Blueberry", "Cherry", "Grapes", "Custard apple", "Dragon fruit", "Guava", "Jackfruit", "Kiwi", "Mango", "Pear", "Oranges", "Papaya", "Peach", "Pomegranate", "Pineapple", "Raspberries", "Strawberries", "Watermelon"],
+    2: ["Computer", "Internet", "Engineering", "Software", "Nanotechnology", "Application", "System", "Robotics", "Electronics", "Communication", "Biotechnology", "Automation", "Information", "Innovation", "Game", "Code", "Technology", "Wireless", "Hardware", "Virtual", "Social media", "Computer science", "Phone", "Process", "Bionics", "Nanotech", "Digital", "Radio", "Value", "Developer", "Core", "Commercial", "Development", "Aeronautical", "Programming Language", "Cybersecurity", "Methodology", "Online", "Technician", "Consumer", "Handheld devices", "Machine", "Protocol", "Architectures", "Engineer", "Invention", "Video"],
+    3: ["Kinetic", "astronomy", "astrophysics", "atom", "beaker", "biochemistry", "biology", "botany", "cell", "chemical", "chemistry", "climate", "climatologist", "data", "datum", "electricity", "electrochemist", "element", "energy", "entomology", "evolution", "experiment", "fossil", "genetics", "geology", "glassware", "cylinder", "gravity", "laboratory", "hypothesis", "magnetism", "mass", "matter", "measure", "meteorologist", "meteorology", "microbiologist", "microbiology", "microscope", "mineral", "mineralogy", "molecule", "motion", "observatory", "observe", "organism"],
+    4: ["Mumbai","Shanghai", "Mexico City", "Karachi", "Istanbul", "Delhi", "Moscow", "Dhaka", "Tokyo", "New York", "London", "Beijing","Honk Kong", "Lahore", "Bangkok", "Kolkata", "Sydney", "Chennai", "Los Angeles", "Melbourne", "Ahmadabad", "Hyderabad", "Singapore", "Berlin", "Montreal", "Kabul", "Pune", "Nairobi", "Stockholm", "Jaipur", "Chicago", "Rome", "Paris", "Philadelphia", "Kuala Lumpur", "Cape Town", "Barcelona", "Budapest", "Washington", "San Fransisco", "Hong Kong"]
 
 }
 
-print("""Choose a category: 
+print("""Choose a category number: 
 1)Fruits 2)Technology 3)Science 4)Places """)
 
 while True:
@@ -39,6 +39,8 @@ while not win:
     for alphabet in word:
         if alphabet.lower() in guessed_alphabet:
             print(alphabet, end=" ")
+        elif alphabet.lower() == " ":
+            print(" ", end=" ")
         else:
             print("_", end=" ")
     print("")
@@ -56,6 +58,6 @@ while not win:
             win = False
 
 if win:
-    print(f"You found the word! It was {word}")
+    print(f"You found the word! It was '{word}'.")
 else:
-    print(f"Game Over! The word was {word}!")
+    print(f"Game Over! The word was '{word}'!")
